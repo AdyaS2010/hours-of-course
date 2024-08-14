3733d2082a27ba9dda0442c2165b8825c0e28cb4
AdyaS2010 <adya.sastry@gmail.com>
2024-08-13 21:34:20 -0500

    Update and rename text-to-grade.c to hours.c

---
+++ hours.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+float calc_hours(int hours[], int weeks, char output);
+
+int main(void)
+{
+    string activity = get_string("Activity: ")
+    int weeks = get_int("Number of weeks participating in %s: ", activity);
+    int hours[weeks];
+
+    for (int i = 0; i < weeks; i++)
+    {
+        hours[i] = get_int("Week %i Hours: ", i);
+    }
+}
+
+// TODO: complete the calc_hours function
+float calc_hours(int hours[], int weeks, char output)
+{
+    float total = 0;
+}
